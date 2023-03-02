74b577d0f6b965c0cb0c528ffaf3732adff8a84b
nicsylvia <nicsylvia15f@gmail.com>
2023-03-02 14:26:27 -0800

    working on the responsive nav

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -25,4 +25,87 @@ const Header = () => {
   )
 }
 
-export default Header
+export default Header;
+
+const Container = styled.div`
+    width: 100%;
+    position: fixed;
+    top: 0;
+    height: 70px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: skyblue;
+    color: white;
+    z-index: 999;
+    span{
+        font-size: 70px;
+        display: none;
+        cursor: pointer;
+        @media screen and (max-width: 500px) {
+            display: block;
+        }
+    }
+`;
+const Wrapper = styled.div`
+    width: 90%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+const Logo = styled.div`
+    cursor: pointer;
+    font-size: 30px;
+`;
+const Navigation = styled.div`
+    display: flex;
+    justify-content: space-around;
+    @media screen and (max-width: 500px) {
+        display: none;
+    }
+`;
+const Nav = styled.div`
+    margin-right: 10px;
+    cursor: pointer;
+    @media screen and (max-width: 500px) {
+        margin-bottom: 20px;
+        border-top: 1px solid white;
+        padding-left: 15px;
+        display: flex;
+        align-items: center;
+    }
+`;
+const Buttons = styled.div`
+    display: flex;
+    @media screen and (max-width: 500px) {
+        display: none;
+    }
+`;
+const Login = styled.div`
+    margin-right: 10px;
+    cursor: pointer;
+    background-color: blue;
+    padding: 10px 15px;
+    border-radius: 5px;
+    @media screen and (max-width: 500px) {
+        width: 150px;
+        margin-left: 10px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+`;
+const SignUp = styled.div`
+    cursor: pointer;
+    background-color: blue;
+    padding: 10px 15px;
+    border-radius: 5px;
+    @media screen and (max-width: 500px) {
+        width: 150px;
+        margin-left: 10px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+`;
