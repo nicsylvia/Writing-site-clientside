20b97c960407fc408d8d30e643f31581183728e0
nicsylvia <nicsylvia15f@gmail.com>
2023-03-02 20:01:41 -0800

    saved all

--- src/Components/Blogs/Hero.tsx
+++ src/Components/Blogs/Hero.tsx
@@ -1,3 +1,18 @@
 import React from 'react'
 import styled from 'styled-components';
 import hero from "../Assets/heroImg.jpg"
+
+const Hero = () => {
+    return (
+      <div>
+          <Container>
+              <Wrapper>
+                  <h1>Contact Me</h1>
+                  <p><a href="/">Home</a> / Send-Me-A-Message</p>
+              </Wrapper>
+          </Container>
+      </div>
+    )
+  }
+  
+  export default Hero;
