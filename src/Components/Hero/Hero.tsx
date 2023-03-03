f261e9b13d544a9687b978f41d699f3085881c98
nicsylvia <nicsylvia15f@gmail.com>
2023-03-02 16:38:56 -0800

    saved all

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -56,5 +56,75 @@ const Hero = () => {
 
 export default Hero;
 
-const Video = styled.video``;
+cconst Input = styled.input`
+background-color: transparent;
+border: none;
+outline: none;
+flex: 1;
+`;
+
+const InputHold = styled.div`
+display: flex;
+align-items: center;
+height: 50px;
+width: 350px;
+background-color: white;
+color: black;
+border-radius: 25px;
+
+@media screen and (max-width: 500px) {
+    width: 280px;
+}
+`;
+const Icon = styled.div`
+margin-left: 10px;
+margin-right: 10px;
+font-size: 20px;
+`;
+
+const ButtonHold = styled.div`
+margin-top: 50px;
+display: flex;
+overflow-x: scroll;
+
+justify-content: center;
+align-items: center;
+width: 100%;
+::-webkit-scrollbar {
+    width: 0;
+}
+
+@media screen and (max-width: 768px) {
+    justify-content: flex-start;
+    align-items: flex-start;
+}
+`;
+
+const Content = styled.div`
+position: absolute;
+height: 100%;
+width: 100%;
+display: flex;
+/* justify-content: center; */
+flex-direction: column;
+align-items: center;
+text-align: center;
+
+h2 {
+    font-size: 30px;
+    margin: 0;
+    margin-top: 30px;
+}
+
+p {
+    width: 600px;
+
+    @media screen and (min-width: 330px) and (max-width: 600px) {
+        width: 320px;
+    }
+    @media screen and (max-width: 320px) {
+        width: 280px;
+    }
+}
+`;
 
