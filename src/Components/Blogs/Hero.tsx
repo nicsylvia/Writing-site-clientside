71d8b4eedddf9832596af1c5e17732d9dda53d8a
nicsylvia <nicsylvia15f@gmail.com>
2023-03-02 20:02:07 -0800

    saved all

--- src/Components/Blogs/Hero.tsx
+++ src/Components/Blogs/Hero.tsx
@@ -16,3 +16,35 @@ const Hero = () => {
   }
   
   export default Hero;
+
+  const Container = styled.div`
+    width: 100%;
+    height: calc(100vh - 70px);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-image: url(${hero});
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    color: white;
+    position: relative;
+    ::before{
+        content: "";
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.83);
+        position: absolute;
+    }
+`;
+const Wrapper = styled.div`
+    z-index: 2;
+    text-align: center;
+    a{
+        text-decoration: none;
+        color: white;
+    }
+    p{
+        color: grey;
+    }
+`;
