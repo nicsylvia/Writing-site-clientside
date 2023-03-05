33726c6b5b3dd413701015b7ca12829c3f86c094
nicsylvia <nicsylvia15f@gmail.com>
2023-03-05 00:34:43 -0800

    saved all

---
+++ src/Components/ApiCalls/APIs.tsx
@@ -0,0 +1,13 @@
+// const UsersEndpoint = "https://api-augustine-blog.onrender.com",
+
+// const BlogsEndpoint = "https://api-augustine-blog.onrender.com"
+
+import React from 'react'
+
+const APIs = () => {
+  return (
+    <div>APIs</div>
+  )
+}
+
+export default APIs
